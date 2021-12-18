f205a02ece9c928ed9f233ee3b514221fb13b216
Wynne-Tran <wynnetran2018@gmail.com>
2021-12-18 18:35:02 -0500

    Finish Header with Arrow Back

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Header from './src/components/Header';
+import {colors, parameters} from "./src/global/styles"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar 
+        barStyle = "light-content"
+        backgroundColor = {colors.StatusBar}
+      
+      />
+      <Header title= "MY ACCOUNT"/>
     </View>
   );
 }
@@ -13,8 +19,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import {colors, parameters} from "../global/styles"
+import {Icon} from 'react-native-elements'
+
+
+const Header = ({title}) => {
+    return (
+        <View style = {styles.header}>
+            <View style = {{marginLeft: 20}}>
+                <Icon 
+                    type = "material-community"
+                    name = "arrow-left"
+                    color = {colors.headerText}
+                    size = {28}
+                    onPress = {() => {}}
+                    
+                />
+                
+            </View>
+            <View>
+                <Text style = {styles.headerText}>{title}</Text>
+            </View>
+        </View>
+    )
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+    header: {
+        flexDirection: "row",
+        backgroundColor: colors.buttons,
+        height: parameters.headerHeight
+    },
+    headerText: {
+        color: colors.headerText,
+        fontSize: 22,
+        fontWeight: "bold",
+        marginLeft: 30,
+    }
+})

68fd189ac04693cca3bd522c5742e8c646fed7fe
Wynne-Tran <wynnetran2018@gmail.com>
2021-12-22 15:51:25 -0500

    Finished Navagation Tab Bottom & Map

---
+++ src/screens/RestaurantMapScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const RestaurantMapScreen = () => {
+    return (
+        <View style = {{flex:1, alignItems:'center', justifyContent:'center'}}>
+            <Text>Restaurant Map</Text>
+        </View>
+    )
+}
+
+export default RestaurantMapScreen
+
+const styles = StyleSheet.create({})

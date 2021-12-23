b5b039040512f04824936527c8a86ff35a34377e
Wynne-Tran <wynnetran2018@gmail.com>
2021-12-23 15:11:06 -0500

    Search Bar & Modal

---
+++ src/components/SearchContent.js
@@ -0,0 +1,179 @@
+import React, {useState, useRef} from 'react'
+import { StyleSheet, Text, View, TouchableWithoutFeedback, Modal, TextInput, FlatList, TouchableOpacity} from 'react-native'
+import { colors } from '../global/styles'
+import {Icon} from 'react-native-elements'
+import * as Animatable from 'react-native-animatable'
+import { useNavigation } from '@react-navigation/native';
+import {filterData} from '../global/Data'
+
+const SearchContent = () => {
+
+    const navigation  = useNavigation();
+    const [data, setData] =  useState([...filterData])
+    const [modalVisible, setModalVisible] = useState(false)
+    const [textInputFossued,setTextInputFossued] = useState(true)
+    const textInput = useRef(0)
+
+    
+    return (
+        <View>
+           <TouchableWithoutFeedback
+                onPress={() => {
+                    setModalVisible(true)
+                }}
+           >
+                <View style = {styles.SearchArea}>
+                    <Icon 
+                        name =  "search"   
+                        style = {styles.searchIcon} 
+                        type ="material"
+                        iconStyle ={{marginLeft:5}}
+                        size = {32}     
+                    />
+                    <Text style = {{fontSize: 15}}> What are you looking for ?</Text>
+                </View>
+           </TouchableWithoutFeedback>
+
+           <Modal
+                animationType='fade'
+                transparent = {false}
+                visible = {modalVisible}
+
+           >
+            <View style = {styles.modal}>
+                <View style = {styles.view1}>
+                    <View style = {styles.TextInput}> 
+                        <Animatable.View>
+                            <Icon 
+                                name = {textInputFossued ? "arrow-back" : "search"}
+                                onPress = {() => {
+                                    if(textInputFossued)
+                                    setModalVisible(false)
+                                    setTextInputFossued(false)
+                                
+                                }}
+                                style = {styles.icon2}
+                                type = "material"
+                                iconStyle = {{marginRight:5}}
+
+                            />
+
+                        </Animatable.View>
+
+                        <TextInput 
+                            style = {{width: "90%", height: 50}}
+                            placeholder=''
+                            autoFocus = {false}
+                            ref = {textInput}
+
+                        />
+
+                        <Animatable.View>
+                            <Icon 
+                                name = {textInputFossued ? "cancel" : null}
+                                iconStyle = {{color: colors.grey3}}
+                                style = {{marginRight: -10}}
+                                type = "material"
+                                onPress = {() => {
+                                    textInput.current.clear()
+                                    //handleSearch()
+                                }}
+
+                            />
+                            
+                        </Animatable.View>
+                    </View>
+                </View>
+                
+                <FlatList 
+                    data = {data}
+                    renderItem = {({item}) => (
+                        <TouchableOpacity 
+                            onPress={() => {
+                                keyboard.dismiss
+                                navigation.navigate("RestaurantSearchScreen", {item: item.name})
+                                setModalVisible(false)
+                                setTextInputFossued(true)
+                            }}
+                        >
+                            <View style={styles.view2}>
+                                <Text style={{color: colors.grey2, fontSize: 15}}>{item.name}</Text>
+                            </View>
+
+                        </TouchableOpacity>
+                    )}
+                    keyExtractor={item => item.id}
+                />
+
+
+            </View>
+           </Modal>
+        </View>
+    )
+}
+
+export default SearchContent
+
+const styles = StyleSheet.create({
+    container :{
+        flex:1
+    },
+
+    text1:{
+        color:colors.grey3,
+        fontSize:16
+    },
+
+    TextInput:{
+        borderWidth:1,
+         borderRadius:12,
+         marginHorizontal:0,
+         borderColor:"#86939e",
+         flexDirection:"row",
+         justifyContent:"space-evenly",
+         alignContent:"center",
+         alignItems:"center",
+         paddingLeft:10,
+         paddingRight:10
+  
+      },
+
+    SearchArea:{marginTop :10,
+        width:"94%",
+        height:50,
+        backgroundColor:colors.grey5,
+        borderRadius:12,
+        borderWidth:1,
+        borderColor:colors.grey4,
+        flexDirection:"row",
+        alignItems:"center"
+      },
+
+    searchIcon:{ fontSize:24,
+                  padding:5,
+                  color:colors.grey2,
+      },
+
+    view1:{ height:70,
+            justifyContent:"center",
+            
+            paddingHorizontal:10,
+      },
+
+    view2:{
+      flexDirection: 'row',
+      padding: 15,
+      alignItems: 'center',
+    },
+
+    icon2 :{ fontSize:24,
+            padding:5,
+            color:colors.grey2,
+    },
+    modal :{
+        flex:1
+    },
+    modal : {
+        flex: 1
+    }
+})
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import SearchContent from '../components/SearchContent'
 
 const SearchScreen = () => {
     return (
-        <View style = {{flex:1, alignItems:'center', justifyContent:'center'}}>
-            <Text>Search Screen</Text>
+        <View style = {{marginStart: 20}}>
+            <SearchContent />
         </View>
     )
 }
 
 export default SearchScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+})
